16777a2898b2ad73db8e5a2a0ccc20e0fac79c1e
tNE1K <21521023@gm.uit.edu.vn>
2024-04-16 13:14:10 +0700

    init login page

--- app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
+++ app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.project_mobile_babycare
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
