1a1751ce750a890a24991f027cfdda383c527231
Bao <21520604@gm.uit.edu.vn>
2024-04-16 16:20:47 +0700

    add sign up page, edit login page, add global value

--- app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
+++ app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
+        setContentView(R.layout.log_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
