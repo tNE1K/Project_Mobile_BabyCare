61e7e3d9d7e998af94c7d4f70ce1fdc3e42594d3
tNE1K <21521023@gm.uit.edu.vn>
2024-04-17 12:30:28 +0700

    thêm spinner vào trang main

--- app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
+++ app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.project_mobile_babycare
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +12,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var hSpinner:Spinner
+    lateinit var baby:Baby
+    lateinit var babyAdapter:BabyAdapter
+    lateinit var babyList:ArrayList<Baby>
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -17,6 +26,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        babyList = ArrayList<Baby>()
+        babyList.add(Baby("Kiên"))
+        babyList.add(Baby("Bảo"))
+        babyList.add(Baby("Nam"))
+        hSpinner = findViewById(R.id.SPNlistBaby)
+        babyAdapter = BabyAdapter(this, babyList)
+        hSpinner.adapter = babyAdapter
+        hSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
         }
 
     }
