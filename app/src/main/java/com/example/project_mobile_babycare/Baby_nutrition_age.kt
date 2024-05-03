ec423ce17dbd4baec83c026a473875fddb35a4b8
tNE1K <21521023@gm.uit.edu.vn>
2024-05-03 20:45:05 +0700

    tạo trang Dinh dưỡng tính theo tuổi bé

--- app/src/main/java/com/example/project_mobile_babycare/Baby_nutrition_age.kt
+++ app/src/main/java/com/example/project_mobile_babycare/Baby_nutrition_age.kt
@@ -1,18 +1,16 @@
 package com.example.project_mobile_babycare
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Baby_nutrition_age : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_baby_nutrition_age)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
