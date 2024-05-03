afa60bbe16b673b7de5d09d15ebd598db55a1cf7
tNE1K <21521023@gm.uit.edu.vn>
2024-05-03 21:49:38 +0700

    chỉnh sửa trang dinh dưỡng tính theo tuổi bé, chỉnh sửa textsize, thêm stroke

--- app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
+++ app/src/main/java/com/example/project_mobile_babycare/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_baby_nutrition_age)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
