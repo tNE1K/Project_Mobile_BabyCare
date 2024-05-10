fc4207734af7d225018850434e136d925d1aca95
tNE1K <21521023@gm.uit.edu.vn>
2024-05-10 22:55:06 +0700

    tạo trang memory và chỉnh sửa textsize button

---
+++ app/src/main/java/com/example/project_mobile_babycare/Baby_Memory_View.kt
@@ -0,0 +1,20 @@
+package com.example.project_mobile_babycare
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Baby_Memory_View : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_baby_memory_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
