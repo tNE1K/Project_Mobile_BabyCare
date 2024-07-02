4309524250dbddb5de781d4986f86c5f296f3e72
tNE1K <21521023@gm.uit.edu.vn>
2024-07-02 16:41:58 +0700

    add datepicker into some pages

--- app/src/main/java/com/example/project_mobile_babycare/MedicalHistoryInput.kt
+++ app/src/main/java/com/example/project_mobile_babycare/MedicalHistoryInput.kt
@@ -1,19 +1,64 @@
 package com.example.project_mobile_babycare
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.isVisible
 
 class MedicalHistoryInput : AppCompatActivity() {
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.medical_history_input)
         enableFullscreenMode()
+        var BTNstartdate: Button = findViewById(R.id.button_start_date)
+        var StartCalendarContainer: FrameLayout = findViewById(R.id.startCalendarContainer)
+        var StartDatePick: DatePicker = findViewById(R.id.startDatePicker)
+
+        var BTNenddate: Button = findViewById(R.id.button_end_date)
+        var EndCalendarContainer: FrameLayout = findViewById(R.id.endCalendarContainer)
+        var EndDatePick: DatePicker = findViewById(R.id.endDatePicker)
+
+        var month: Int
+        BTNstartdate.setOnClickListener() {
+            if(EndCalendarContainer.isVisible)
+                EndCalendarContainer.visibility = View.GONE
+
+            if (!StartCalendarContainer.isVisible)
+                StartCalendarContainer.visibility = View.VISIBLE
+            else StartCalendarContainer.visibility = View.GONE
+        }
+
+        StartDatePick.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            month = monthOfYear + 1
+            val msg = "$dayOfMonth/$month/$year"
+            BTNstartdate.setText(msg)
+        }
+
+        BTNenddate.setOnClickListener() {
+            if(StartCalendarContainer.isVisible)
+                StartCalendarContainer.visibility = View.GONE
+
+            if (!EndCalendarContainer.isVisible)
+                EndCalendarContainer.visibility = View.VISIBLE
+            else EndCalendarContainer.visibility = View.GONE
+        }
+
+        EndDatePick.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            month = monthOfYear + 1
+            val msg = "$dayOfMonth/$month/$year"
+            BTNenddate.setText(msg)
+        }
     }
 
     //hide system bar
