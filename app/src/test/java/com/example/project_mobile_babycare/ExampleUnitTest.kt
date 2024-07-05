d72bd7fc6c9d63f3b6c178c6a873fdd57656fdd9
Bao <21520604@gm.uit.edu.vn>
2024-07-05 16:27:41 +0700

    reformat project

--- app/src/test/java/com/example/project_mobile_babycare/ExampleUnitTest.kt
+++ app/src/test/java/com/example/project_mobile_babycare/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.project_mobile_babycare
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
