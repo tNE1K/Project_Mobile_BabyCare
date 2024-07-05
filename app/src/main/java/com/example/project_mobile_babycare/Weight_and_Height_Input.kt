ec3d9b88da11f32cf95936ef1c81c180c064667e
Bao <21520604@gm.uit.edu.vn>
2024-07-05 16:24:42 +0700

    fix datepicker in weight and height input

--- app/src/main/java/com/example/project_mobile_babycare/Weight_and_Height_Input.kt
+++ app/src/main/java/com/example/project_mobile_babycare/Weight_and_Height_Input.kt
@@ -2,16 +2,20 @@ package com.example.project_mobile_babycare
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.core.view.isVisible
+import androidx.fragment.app.DialogFragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.Calendar
 
 class Weight_and_Height_Input : AppCompatActivity() {
     @SuppressLint("NewApi")
@@ -19,20 +23,16 @@ class Weight_and_Height_Input : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weight_and_height_input)
         enableFullscreenMode()
-        var BTNdateInput: Button = findViewById(R.id.btn_dateinput)
-        var CalendarContainer: FrameLayout = findViewById(R.id.calendarContainer)
-        var DatePick: DatePicker = findViewById(R.id.datePicker)
-        var month: Int
-        BTNdateInput.setOnClickListener {
-            if (!CalendarContainer.isVisible)
-                CalendarContainer.visibility = View.VISIBLE
-            else CalendarContainer.visibility = View.GONE
-        }
 
-        DatePick.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
-            month = monthOfYear + 1
-            val msg = "$dayOfMonth/$month/$year"
-            BTNdateInput.text = msg
+        val auth = Firebase.auth
+        val user = auth.currentUser
+        val db = Firebase.firestore
+
+        var BTNdateInput: Button = findViewById(R.id.btn_dateinput)
+
+        BTNdateInput.setOnClickListener {
+            val newFragment = DatePickerFragment()
+            newFragment.show(supportFragmentManager, "datePicker")
         }
     }
 
@@ -44,6 +44,23 @@ class Weight_and_Height_Input : AppCompatActivity() {
             it.hide(WindowInsetsCompat.Type.systemBars())
             it.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            // Use the current date as the default date in the picker.
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            return DatePickerDialog(requireContext(), this, year, month, day)
+        }
+
+        override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+            val activity = activity as? Weight_and_Height_Input
+            activity?.findViewById<Button>(R.id.btn_dateinput)?.text = "$day/${month + 1}/$year"
         }
     }
 }
